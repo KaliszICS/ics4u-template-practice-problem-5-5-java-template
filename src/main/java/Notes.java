574877260afb4864b8443e8776adb397f9015f49
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-05-20 08:51:17 -0400

    Update Notes.java

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -1,6 +1,28 @@
 
 class Notes {
 	public static void main(String[] args) {
+
+		Table[] tables = new Table[5];
+
+		tables[0] = new Table(1, 5, 3);
+		tables[1] = new Table(5, 2, 2);
+		tables[2] = new Table(4, 5, 3);
+		tables[3] = new Table(2, 1, 4);
+		tables[4] = new Table(3, 2, 1);
+
+		//unsorted table array
+		for (int i = 0; i < tables.length; i++) {
+			System.out.println(tables[i]);
+		}
+
+		System.out.println("----------------------------------------------");
+		Arrays.sort(tables);
+
+		//sorted by comparable table array
+		for (int i = 0; i < tables.length; i++) {
+			System.out.println(tables[i]);
+		}
+		
 		Animal a = new Dog();
 		Animal b = new Cat();
 		Animal c = new Horse();
