e20adc0c817e82538437eeb99a5a9ce04b34b39b
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-05-20 08:49:40 -0400

    Add files via upload

---
+++ src/main/java/Lesson.java
@@ -0,0 +1,55 @@
+class Lesson {
+	public static void run() {
+		Animal a = new Dog();
+		Animal b = new Cat();
+		Animal c = new Horse();
+
+		a.talk();
+		b.talk();
+		c.talk();
+
+		Cat cat = new Cat();
+		Dog dog = new Dog();
+		Horse horse = new Horse();
+
+		a = cat;
+		b = dog;
+		c = horse;
+
+		a.talk();
+		b.talk();
+		c.talk();
+
+		animalTalk(cat);
+		animalTalk(a);
+		animalTalk(dog);
+		animalTalk(b);
+		animalTalk(horse);
+		animalTalk(c);
+
+		// moveAnimal(a);
+		moveAnimal(cat);
+		// moveAnimal(b);
+		moveAnimal(dog);
+		// moveAnimal(c);
+		moveAnimal(horse);
+
+
+		//instanceof
+		//returns a boolean if the first object is the same as the second object.  Or if the second object is a subclass of the first object.
+
+
+		System.out.println(a instanceof Animal); //true
+		System.out.println(a instanceof Dog); //false
+		System.out.println(a instanceof Cat); //true
+		System.out.println(a instanceof Moveable); //true
+	}
+
+	public static void animalTalk(Animal a) {
+		a.talk();
+	}
+
+	public static void moveAnimal(Moveable m) {
+		m.move();
+	}
+}
